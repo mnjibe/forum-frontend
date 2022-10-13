ae3f7909a11de0b491f9dfeabf11cc94693b2e94
Micah Njibe <mnjibe11@gmail.com>
2022-10-13 14:31:15 -0400

    working axios.get

--- src/App.js
+++ src/App.js
@@ -11,32 +11,40 @@ import Input from '@mui/material/Input';
 import SearchIcon from '@mui/icons-material/Search';
 import Posts from '../src/components/Posts'
 
-const exPosts = [
-    { title: "test 1" },
-    { title: "test 2" },
-    { title: "test 3" }
-]
-
 function App() {
-    const [posts, setPosts] = React.useState(null)
-
+    const [data, setPosts] = React.useState([])
     React.useEffect(() => {
-        axios.get('http://localhost:6006/api/v1/posts').then(function(res) {
-            // console.log('res: ', res)
-            setPosts(res.data)
-        }).catch(function(err) {
-            console.log('err: ', err)
-
-        })
+        axios.get('http://localhost:6006/api/v1/posts')
+        .then(res=> {
+            console.log('Pulling From::  ', res.data.data)
+            setPosts(res.data.data)})
+        .catch(err => console.log('err::  ', err))
+    
     }, [])
-
-    console.log('posts: ! : !:', posts)
-
+    console.log(data)
+    const arr = data.map((data,index) => {
+        return (
+            <tr> 
+                <td> {data.id}</td>
+                <td> {data.title}</td>
+                <td> {data.body}</td>  
+            </tr>
+        )
+    })
+    
     return (
         <div>
-            <Posts posts={exPosts} />
+         <h1> View Posts</h1>   
+         <table>
+            <tr>
+            <th> ID </th>    
+            <th> Title </th>
+            <th> Body </th>
+            </tr> 
+            {arr}
+            </table>
         </div>
-
+    
     )
-}
-export default App;
+    }
+    export default App;
